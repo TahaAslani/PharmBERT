b0b00effdc1fc0c279981ce7dd2b7ec7b47b6628
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-04-27 02:46:28 -0400

    Update 1-install-dependancies.sh

--- 1-install-dependancies.sh
+++ 1-install-dependancies.sh
@@ -1,8 +1,9 @@
 pip install orderedset
 pip install pytorch
-pip install transformers
+pip install transformers==4.13.0
 pip install evaluate
 pip install protobuf==3.20.*
+pip install git+https://github.com/huggingface/accelerate
 
 # Download Transformers codes
 wget https://github.com/huggingface/transformers/archive/refs/heads/main.zip
