7d2f571be93f2ce3e9c126661188ae72d77d5d4c
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-04-27 03:02:36 -0400

    Create ARD.sh

---
+++ ARD.sh
@@ -0,0 +1,34 @@
+data_path=$2
+train_file=$data_path/train.json
+validation_file=$data_path/test.json
+
+per_device_train_batch_size=16
+learning_rate=0.0001
+max_length=128
+seed=42
+
+# Select the model to train
+if [[ $1 == "cased" ]]; then
+     model_path='bert-base-cased'
+elif [[ $1 == "uncased" ]]; then
+     model_path='bert-base-uncased'
+else 
+     echo "Either select cased or uncased model!"
+     exit $exit_code
+fi
+
+output_dir='ARD-'$1
+
+
+mkdir $output_dir
+
+
+python transformers-4.14.1-release/examples/pytorch/token-classification/run_ner_no_trainer.py \
+  --model_name_or_path $model_path \
+  --train_file $train_file \
+  --validation_file $validation_file \
+  --output_dir $output_dir \
+  --per_device_train_batch_size $per_device_train_batch_size \
+  --learning_rate $learning_rate \
+  --max_length $max_length \
+  --seed $seed
