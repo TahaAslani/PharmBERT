9ba11e3676c33322f313b10fd22a1a848c888db0
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-04-28 11:20:37 -0400

    Update Eval.py

--- DDI/Eval.py
+++ DDI/Eval.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  7 23:27:58 2022
-
-@author: taha
-"""
-
 import os
 import sys
 import numpy as np
@@ -17,7 +9,6 @@ folder = sys.argv[1]
 preds = np.load(os.path.join(folder,'preds.npy'))
 labels = np.load(os.path.join(folder,'labels.npy'))
 
-
 generic_label = 0
 interest_labels = list(set(labels))
 interest_labels.remove(generic_label)
@@ -25,37 +16,7 @@ interest_labels.remove(generic_label)
 F1 = f1_score(labels, preds, labels=interest_labels, average='micro')
 print('Cllassification F1-micro',F1)
 
-F1_macro = f1_score(labels, preds, labels=interest_labels, average='macro')
-print('Cllassification F1-macro',F1_macro)
-
-
-f = open(os.path.join(folder,'MICRO-F1-CLASS.txt'),'w')
+f = open(os.path.join(folder,'MICRO-F1.txt'),'w')
 f.write(str(np.round(F1,5)))
 f.write('\n')
 f.close()
-
-
-f = open(os.path.join(folder,'MACRO-F1-CLASS.txt'),'w')
-f.write(str(np.round(F1_macro,5)))
-f.write('\n')
-f.close()
-
-
-labels_DDI=labels[labels!=0]
-preds_DDI=preds[labels!=0]
-
-F1_r = f1_score(labels_DDI, preds_DDI, average='micro')
-F1_macro_r = f1_score(labels_DDI, preds_DDI, average='macro')
-
-f = open(os.path.join(folder,'MICRO-F1-REMOVE.txt'),'w')
-f.write(str(np.round(F1_r,5)))
-f.write('\n')
-f.close()
-
-
-f = open(os.path.join(folder,'MACRO-F1-REMOVE.txt'),'w')
-f.write(str(np.round(F1_macro_r,5)))
-f.write('\n')
-f.close()
-
-
