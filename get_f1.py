3cab09b5ae67a49b5a0a1c0b501f3faebf60b4b4
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-04-28 11:44:10 -0400

    Update get_f1.py

--- get_f1.py
+++ get_f1.py
@@ -4,10 +4,13 @@ import numpy as np
 import sys
 import os
 
+# differetn seeds
 seeds = [42, 555, 666, 999, 9999]
 
+# get the directory
 res_path = sys.argv[1]
 
+# Read json file
 def read_json(path):
     f = open(path)
     lines = f.readlines()
@@ -18,13 +21,14 @@ def read_json(path):
             
     return f1
 
+# Collect F1 of differetn seeds
 F1_list = []
-
 for seed in seeds:
 
     f1_path = os.path.join(res_path, str(seed), 'eval-micro-.json')
     f1 = read_json(f1_path)
     F1_list.append(f1)
 
+# Report
 print('Mean F1', np.mean(F1_list))
 print('Standard error F1', sem(F1_list))
