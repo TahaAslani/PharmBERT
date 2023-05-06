78d045394d2b5fbcfa8b5bfc30c7d5612043ee49
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-05-05 21:03:11 -0400

    Update 1-install-dependancies.sh

--- 1-install-dependancies.sh
+++ 1-install-dependancies.sh
@@ -1,16 +1,18 @@
 conda install python=3.8.10 -y # Or use another way to install this version of python
 
+# Download Transformers codes
+wget https://github.com/huggingface/transformers/archive/refs/heads/v4.14.1-release.zip
+unzip v4.14.1-release.zip
+rm v4.14.1-release.zip
+
+# PIP
 pip install orderedset
 pip install pytorch
 pip install seqeval
 pip install evaluate
 pip install lxml
 python -m pip install scipy
-pip install transformers==4.14.1
 pip install protobuf==3.20.*
 pip install git+https://github.com/huggingface/accelerate
-
-# Download Transformers codes
-wget https://github.com/huggingface/transformers/archive/refs/heads/v4.14.1-release.zip
-unzip v4.14.1-release.zip
-rm v4.14.1-release.zip
+pip install transformers==4.14.1
+pip install 'urllib3<2'
